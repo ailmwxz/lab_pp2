fe338245b7819130686488f3729cb467b30730d0
Shunan Altynai <a_shunan@kbtu.kz>
2025-01-25 16:58:02 +0500

    Add files via upload

---
+++ lab2/pyth distionaries/Loop Dictionari.py
@@ -0,0 +1,10 @@
+for x in thisdict.values():
+  print(x)
+
+
+for x, y in thisdict.items():
+  print(x, y)
+
+
+for x in thisdict.keys():
+  print(x)
---
+++ lab2/pyth lists/access list Items.py
@@ -0,0 +1,11 @@
+#1
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+#2
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+#3
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
